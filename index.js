7b2b05bed39fa547becc2898376a050a8282ffcc
Joe Longstreet <joelongstreet@gmail.com>
2014-07-21 13:18:11 -0500

    poll the dow jones for market price updates

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var path = require('path');
 var express = require('express');
 var spark = require('sparknode');
 var http = require('http');
+var needle = require('needle');
 
 var app = express();
 var server = http.Server(app);
@@ -15,15 +16,13 @@ var core = new spark.Core({
 });
 
 core.on('connect', function(e){
-  console.log(e);
-  console.log('connected');
+  console.log('Spark Connected:', e);
 });
 
 app.get('/notify-spark', function(req, res){
   var params = req.query.directive + ',' + req.query.color;
   core.notify(params, function(err, data){
     if(err) console.log(err);
-    console.log(data);
   });
 
   res.send({ ok:true });
@@ -32,3 +31,48 @@ app.get('/notify-spark', function(req, res){
 server.listen(port, function(){
   console.log('server started, listening on port ' + port + '...');
 });
+
+
+
+var djURL = 'http://betawebapi.dowjones.com/fintech/data/api/v1/quotes/dji';
+var getDowJones = function(){
+  console.log('checking dow jones...');
+  needle.get(djURL, function(error, response, body){
+    if (!error && response.statusCode == 200){
+      var price = null;
+      try{ price = body.CompositeTrading.Last.Price.Value; }
+      catch(err){ console.log(err); }
+
+      if(price) priceUpdate(price);
+    }
+  });
+};
+
+
+var lastPrice = 0;
+var priceUpdate = function(price){
+  console.log('comparing price...', price, 'vs', lastPrice);
+
+  var params = null;
+  if(price > lastPrice)
+    params = '1,green';
+  else if(price < lastPrice)
+    params = '1,red';
+
+  lastPrice = price;
+
+  if(params){
+    core.notify(params, function(err, data){
+      if(err) console.log(err);
+    });
+
+    setTimeout(function(){
+      core.notify('0,blue', function(err, data){
+        if(err) console.log(err);
+      });
+    }, 3000);
+  }
+};
+
+
+setInterval(getDowJones, 5000);
