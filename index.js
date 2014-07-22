7eea5be20a5ae0293f496ae720edcc28d472e66b
Joe Longstreet <joelongstreet@gmail.com>
2014-07-21 21:03:43 -0500

    break out into more modular components

--- index.js
+++ index.js
@@ -1,88 +1,32 @@
 var path = require('path');
 var express = require('express');
-var spark = require('sparknode');
 var http = require('http');
 var needle = require('needle');
+var spark = require('./libs/spark');
+var dj = require('./libs/dow-jones');
 
 var app = express();
 var server = http.Server(app);
 var port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+spark.connect();
+dj.poll();
 
-var core = new spark.Core({
-  accessToken: process.env.SPARK_TOKEN,
-  id: process.env.SPARK_ID
-});
-
-core.on('connect', function(e){
-  console.log('Spark Connected:', e);
-});
 
 app.get('/notify-spark', function(req, res){
   var params = req.query.directive + ',' + req.query.color;
-  core.notify(params, function(err, data){
-    if(err) console.log(err);
-  });
+  spark.notify(params);
 
   if(req.query.directive == '1'){
     setTimeout(function(){
-      core.notify('0,blue', function(err, data){
-        if(err) console.log(err);
-      });
+      spark.notify('0, blue');
     }, 3000);
   }
 
-  res.send({ ok:true });
+  res.send({ ok: true });
 });
 
 server.listen(port, function(){
   console.log('server started, listening on port ' + port + '...');
 });
-
-
-
-var djURL = 'http://betawebapi.dowjones.com/fintech/data/api/v1/quotes/dji';
-var getDowJones = function(){
-  console.log('checking dow jones...');
-  needle.get(djURL, function(error, response, body){
-    if (!error && response.statusCode == 200){
-      var currentPrice = null;
-      var openingPrice = null;
-      try{
-        currentPrice = body.CompositeTrading.Last.Price.Value;
-        openingPrice = body.CompositeTrading.Open.Value;
-      }
-      catch(err){ console.log(err); }
-
-      if(currentPrice && openingPrice)
-        priceUpdate(openingPrice, currentPrice);
-    }
-  });
-};
-
-
-var priceUpdate = function(openingPrice, currentPrice){
-  console.log('comparing price...', openingPrice, 'vs', currentPrice);
-
-  var params = null;
-  if(currentPrice > openingPrice)
-    params = '1,white';
-  else if(currentPrice < openingPrice)
-    params = '1,yellow';
-
-  if(params){
-    core.notify(params, function(err, data){
-      if(err) console.log(err);
-    });
-
-    setTimeout(function(){
-      core.notify('0,blue', function(err, data){
-        if(err) console.log(err);
-      });
-    }, 3000);
-  }
-};
-
-
-setInterval(getDowJones, 60000);
---
+++ libs/dow-jones.js
@@ -0,0 +1,60 @@
+var core = require('./libs/core');
+var djURL = 'http://betawebapi.dowjones.com/fintech/data/api/v1/quotes/dji';
+
+
+var getDowJones = function(){
+  console.log('checking dow jones...');
+
+  needle.get(djURL, function(error, response, body){
+    if (!error && response.statusCode == 200){
+      var currentPrice = null;
+      var openingPrice = null;
+      try{
+        currentPrice = body.CompositeTrading.Last.Price.Value;
+        openingPrice = body.CompositeTrading.Open.Value;
+      }
+      catch(err){ console.log(err); }
+
+      if(currentPrice && openingPrice)
+        priceUpdate(openingPrice, currentPrice);
+    }
+  });
+};
+
+
+var priceUpdate = function(openingPrice, currentPrice){
+  console.log('comparing price...', openingPrice, 'vs', currentPrice);
+
+  var params = null;
+  if(currentPrice > openingPrice)
+    params = '1,white';
+  else if(currentPrice < openingPrice)
+    params = '1,yellow';
+
+  if(params){
+    core.notify(params, function(err, data){
+      if(err) console.log(err);
+    });
+
+    setTimeout(function(){
+      core.notify('0,blue', function(err, data){
+        if(err) console.log(err);
+      });
+    }, 3000);
+  }
+};
+
+
+var poll = null;
+var startPoll = function(){
+  poll = setInterval(getDowJones, 60000);
+};
+
+
+var stopPoll = function(){
+  clearInterval(poll);
+};
+
+
+exports.startPoll = startPoll;
+exports.stopPoll = stopPoll;
