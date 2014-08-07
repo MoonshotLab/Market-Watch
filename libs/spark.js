e42a78f5f5c06f891578424d2421270e6da62599
Joe Longstreet <joelongstreet@gmail.com>
2014-08-07 10:59:47 -0500

    reconnect freals, passback errors correctly

--- libs/spark.js
+++ libs/spark.js
@@ -1,21 +1,21 @@
 var spark = require('sparknode');
-
-var core = new spark.Core({
-  accessToken: process.env.SPARK_TOKEN,
-  id: process.env.SPARK_ID
-});
-
-
-core.on('error', function(e){
-  console.log(e);
-});
+var core = null;
 
 
 var connect = function(){
+  core = new spark.Core({
+    accessToken: process.env.SPARK_TOKEN,
+    id: process.env.SPARK_ID
+  });
+
   core.on('connect', function(e){
     if(e.connected === false) connect();
     else
       console.log('Spark Connected:', e);
+  });
+
+  core.on('error', function(e){
+    console.log(e);
   });
 };
 
@@ -24,7 +24,7 @@ var notify = function(params, next){
   try{
     core.notify(params, function(err, data){
       if(err) console.log(err);
-      if(next) next(data);
+      if(next) next(err, data);
     });
   } catch(e){
     connect();
